a40bf7c91ce4732b294b17c3ebc177e5042b6ad4
ednaustin <edramirez512@gmail.com>
2019-07-19 22:57:26 -0500

    changed static files root

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,8 +126,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'portfolio/static')
+STATIC_URL = 'portfolio/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
